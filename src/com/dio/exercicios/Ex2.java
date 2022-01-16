e9c9bb5183fc4e2aafb671910a245bd73e44a5f9
Cuckoo-Head <cuckoohead@disroot.org>
2022-01-16 14:39:35 -0300

    exercicio Ex2 resolvido

--- src/com/dio/exercicios/Ex2.java
+++ src/com/dio/exercicios/Ex2.java
@@ -1,10 +1,41 @@
 package com.dio.exercicios;
 
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Iterator;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
 
+		Queue<String> fila = new LinkedList<>();
 		
+		fila.add("Juliana");
+		fila.add("Pedro");
+		fila.add("Carlos");
+		fila.add("Larissa");
+		fila.add("João");
+		System.out.println("Crie uma fila: " + fila);
+		
+		Iterator<String> iteratorFila = fila.iterator();
+		System.out.println("Navegue pela fila e exiba os itens: ");
+		while(iteratorFila.hasNext()) {
+			System.out.println(">" + iteratorFila.next());
+		}
+		
+		System.out.println("Primeiro elemento: " + fila.peek());
+		
+		System.out.println("Primeiro elemento, removido após mostrar: " + fila.poll());
+		System.out.println(fila);
+		
+		fila.add("Daniel");
+		System.out.println("Daniel adicionado: " + fila);
+		
+		System.out.println("Fila possui " + fila.size() + " elementos.");
+		
+		System.out.println("A fila está vazia? " + fila.isEmpty());
+		
+		System.out.println("A fila possui um item 'Carlos'? " + fila.contains("Carlos"));
 	}
 
 }
